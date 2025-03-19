14661783bb8f7f79a22a0235f754136b6af8a893
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-19 11:19:40 -0300

    11-Standard components
    11-Standard components

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
-import Link from "next/link"
+import Nav from "@/components/nav"
 
 export default function HomePage(){
   return(
     <>
       <h1>I am home</h1>
-      <ul>
-        <li>
-          <Link href="/users">Users</Link>
-        </li>
-        <li>
-          <Link href="/users/profile/66">Users 66</Link>
-        </li>
-      </ul>
+      <Nav/>
     </>
   )
 }
